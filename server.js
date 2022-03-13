5c6e4a945aa7528cccdd4ab918257136dce9072e
Marissa Fandel <mfandel@alumni.nd.edu>
2022-03-12 19:14:27 -0700

    created routes directory & added initial pseudo code

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-// Declare variables / Bring in express
+// Bring in Express
 const express = require('express');
 const app = express();
+
+// Declare port variable
 const PORT = 3001;
+
+// Bring in routes
+
+
+// Route middleware
+
+
+// Listener to begin server
