c3fe07e3cdca3af601e8c1f015c074c2a85b905a
Marissa Fandel <mfandel@alumni.nd.edu>
2022-03-13 15:03:27 -0600

    reverted POST route to previous

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,11 +12,11 @@ router.get('/api/notes', (req, res) => {
     console.info(`${req.method} request received for notes`);
     // res.status(200).json(notes);
     readFromFile(notes)
-    .then((notes) => res.json(JSON.parse(notes)));
+    .then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route to add new note to db
-router.post('/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to add a note`);
 
     // Destructuring assignment for items in req.body
