d7ee986031972340ed09eb8d65505275b8d3bde5
Marissa Fandel <mfandel@alumni.nd.edu>
2022-03-12 22:37:26 -0700

    added some pseudo code to api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,15 +1,28 @@
-// Import Express & Path & Declare Router
+// Import Express, Path, & UUID & Declare Router
 const express = require('express');
-const notesRouter = express.Router();
+const router = express.Router();
 const path = require('path');
+const uuid = require('../utils/uuid');
 
+// GET Route for all notes in db
+router.get('/notes', (req, res) =>
+  
+);
 
+// POST Route to add new note to db
+router.post('/notes', (req, res) =>
+  
+);
+
+// New note object
 const newNote = {
     title,
     text,
-    
+    note_id: uuid()
 }
+
+// DELETE Route to delete note from db
 
 
 // Export router
-module.exports = notesRouter;
+module.exports = router;
--- utils/uuid.js
+++ utils/uuid.js
@@ -1,4 +1,5 @@
 // Export function to generate random alpha-numeric string for Note ID value
+
 const uuid = () =>
     Math.floor((1 + Math.random()) * 0x10000)
         .toString(16)
