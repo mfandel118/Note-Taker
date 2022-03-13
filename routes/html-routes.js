18294280a4faa91fa8d6610b91f08cc1bdba3b0d
Marissa Fandel <mfandel@alumni.nd.edu>
2022-03-12 21:32:31 -0700

    added Get Route for notes.html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const path = require('path');
 
+//GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 // GET Route for landing page
 app.get('*', (req, res) =>
