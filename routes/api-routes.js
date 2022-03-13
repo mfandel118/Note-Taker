e53ce91586de15684507ed640b9e04d80db247c5
Marissa Fandel <mfandel@alumni.nd.edu>
2022-03-12 23:13:24 -0700

    added get route for all notes in api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,17 +1,19 @@
-// Import Express, Path, & UUID & Declare Router
+// Imports & Router Declaration
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const fs = require('fs');
 const uuid = require('../utils/uuid');
+const notes = require('../db/db.json')
 
 // GET Route for all notes in db
-router.get('/notes', (req, res) =>
-  
+router.get('/api/notes', (req, res) =>
+    res.status(200).json(notes)
 );
 
 // POST Route to add new note to db
-router.post('/notes', (req, res) =>
-  
+router.post('/api/notes', (req, res) =>
+
 );
 
 // New note object
@@ -22,7 +24,9 @@ const newNote = {
 }
 
 // DELETE Route to delete note from db
-
+router.delete('/api/notes', (req, res) =>
+    
+);
 
 // Export router
 module.exports = router;
